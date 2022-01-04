62826459defd4e0bfd88ba18bbbff36c9a0b72c2
Rohit Raj <rohitraj.shavili@gmail.com>
2022-01-04 19:59:07 +0530

    out commit

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -1,10 +1,10 @@
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}"
+  dns_prefix          = var.resource_group_name
   location            = azurerm_resource_group.aks_rg.location
-  name                = "${azurerm_resource_group.aks_rg.name}-cluster"
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  name                = var.resource_group_name-cluster
+  resource_group_name = var.resource_group_name
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
-  node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+  node_resource_group = var.resource_group_name-nrg
 
 
   default_node_pool {
