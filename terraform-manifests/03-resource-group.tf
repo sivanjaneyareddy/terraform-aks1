5e75bf6e2fa90cc59d1f750c585693d7c194d4d8
Rohit Raj <rohitraj.shavili@gmail.com>
2022-01-04 17:32:43 +0530

    rg commit

--- terraform-manifests/03-resource-group.tf
+++ terraform-manifests/03-resource-group.tf
@@ -1,7 +1,5 @@
 # Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
-/*
 resource "azurerm_resource_group" "aks_rg" {
   name = "RG-Container"
   location = var.location
 }
-*/
