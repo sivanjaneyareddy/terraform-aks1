5ebfcdc94fb66e6c44480de87a8de5c7780b150e
Rohit Raj <rohitraj.shavili@gmail.com>
2022-01-04 20:21:47 +0530

    out commit

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -2,6 +2,6 @@
 resource "azuread_group" "aks_administrators" {
   #name        = "${azurerm_resource_group.aks_rg.name}-administrators"
   display_name        = "${var.resource_group_name}-administrators"
-  description = "Azure AKS Kubernetes administrators for the "${var.resource_group_name}" cluster."
+  description = "Azure AKS Kubernetes administrators for the cluster."
   security_enabled = true
 }
